77dc341723504ba675f6416ef4eab28e633d2720
Greg Moss <gjmoss@gmail.com>
2017-11-18 22:23:29 +0000

    added ch2 q4

---
+++ ch2/question4.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Write a PHP program that computes the total cost of this restaurant meal: two hamburgers at $ 4.95 each, one chocolate milkshake at $ 1.95, 
+and one cola at 85 cents. The sales tax rate is 7.5%, and you left a pre-tax tip of 16%.
+Sklar, David. Learning PHP: A Gentle Introduction to the Web's Most Popular Language (Kindle Locations 952-954). O'Reilly Media. Kindle Edition. 
+*/
+header("Content-Type: text/plain");
+$vorname = "greg";
+$surname = "moss";
+$name = $vorname." ".$surname;
+print $name."\n";
+print strlen($name);
+
+?>
