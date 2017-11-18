4adb480a2d17101fe646231da082bc3a9e869dd8
Greg Moss <gjmoss@gmail.com>
2017-11-18 22:16:11 +0000

    completed ch2 q3

--- ch2/question3.php
+++ ch2/question3.php
@@ -8,15 +8,21 @@ header("Content-Type: text/plain");
 $hamburger_cost = 4.95;
 $milkshake_cost = 1.95;
 $cola_cost = 0.85;
-$tip = 0.16;
-$tax = 0.075;
+$tipRate = 0.16;
+$taxRate = 0.075;
 
 $cost = 2 * $hamburger_cost + $milkshake_cost + $cola_cost;
-$tip *= $cost;
-$tax *= $cost;
+$tip = $cost * $tipRate;
+$tax = $cost * $taxRate;
 
-print "Food \t = $cost\n";
-print "Tax \t = $tax\n";
-print "Tip \t = $tip\n";
-printf("Total \t = $%.2f\n",$cost+$tax+$tip);
+
+print "Food\n";
+printf ("Hamburger \t 2 @ $hamburger_cost \t = $%.2f\n",$hamburger_cost * 2);
+printf ("Milkshake \t 1 @ $milkshake_cost \t = $%.2f\n",$milkshake_cost);
+printf ("Cola \t\t 1 @ $cola_cost \t = $%.2f\n\n",$cola_cost);
+printf ("Tax \t\t @ %.1f%% \t = $%.2f\n",$taxRate*100, $tax);
+printf ("Tip \t\t @ %.1f%% \t = $%.2f\n",$tipRate*100, $tip);
+
+printf("***** Grand Total \t\t = $%.2f *****\n",$cost+$tax+$tip);
+print "Thanks for your custom y'all";
 ?>
